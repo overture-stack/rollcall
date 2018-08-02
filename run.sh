ea744ad79fc50867c0cbe742607c2ecad7e551d5
rtisma <rtisma@gmail.com>
2018-08-02 19:26:18 -0400

    added build and run scripts

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+server_port=8080
+network_id=default
+elasticsearch_host=kf-es
+elasticsearch_port=9300
+elasticsearch_custer_name=kf-es
+image_name=rollcall-1.0
+
+docker run \
+    -p $server_port:8080 \
+    --net=$network_id \
+    -e "ES_HOST=$elasticsearch_host" \
+    -e "ES_PORT=$elasticsearch_port" \
+    -e "ES_CLUSTER_NAME=$elasticsearch_custer_name" \
+    $image_name
