d539a51e7bc44e424ba3d42ac7f885f3f0323372
rtisma <rtisma@gmail.com>
2018-08-02 19:35:23 -0400

    updated

--- docker/run.sh
+++ docker/run.sh
@@ -6,11 +6,15 @@ elasticsearch_host=kf-es
 elasticsearch_port=9300
 elasticsearch_custer_name=kf-es
 image_name=rollcall-1.0
+container_name=rollcall-server
 
 docker run \
+    -d \
+    --rm \
     -p $server_port:8080 \
     --net=$network_id \
     -e "ES_HOST=$elasticsearch_host" \
     -e "ES_PORT=$elasticsearch_port" \
     -e "ES_CLUSTER_NAME=$elasticsearch_custer_name" \
+    --name $container_name
     $image_name
