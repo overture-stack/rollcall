b48ac76277b4fe0d93c54a33c1e6a6dba85b1459
jaserud <udjaser@gmail.com>
2020-03-13 15:01:46 -0400

    remove redundant annotation

--- src/main/java/bio/overture/rollcall/config/ElasticsearchConfig.java
+++ src/main/java/bio/overture/rollcall/config/ElasticsearchConfig.java
@@ -18,7 +18,6 @@
 
 package bio.overture.rollcall.config;
 
-import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.val;
 import org.apache.http.HttpHost;
@@ -28,30 +27,28 @@ import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.net.URL;
 
 @Configuration
-@ConfigurationProperties("elasticsearch")
 public class ElasticsearchConfig {
 
   @Value("${elasticsearch.host}")
-  @NonNull private String host;
+  private String host;
 
   @Value("${elasticsearch.port}")
-  @NonNull private int port;
+  private int port;
 
   @Value("${elasticsearch.authEnabled}")
-  @NonNull private boolean authEnabled;
+  private boolean authEnabled;
 
   @Value("${elasticsearch.user}")
-  @NonNull private String user;
+  private String user;
 
   @Value("${elasticsearch.password}")
-  @NonNull private String password;
+  private String password;
 
   @Bean
   @SneakyThrows
